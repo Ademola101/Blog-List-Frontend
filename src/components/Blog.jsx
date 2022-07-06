2ded4f09ada4e9aa94e57b60a5c9889f4fe6763d
Ademola <ademolaogunmokun@yahoo.com>
2022-07-06 11:47:22 +0100

    style

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,6 +1,14 @@
 import {useState} from 'react';
 
 const Blog = ({blog}) => {
+  
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
 
 const [showAll, setShowAll] = useState(false);
 
@@ -9,9 +17,9 @@ setShowAll(!showAll)
 }
 
   return (
-    <div>
+    <div style={blogStyle}>
       <div> Title: 
-      {blog.title} <button onClick={onClick} > Show {showAll ? 'less' : 'details'} </button>
+      {blog.title} <button onClick={onClick} > {showAll ? 'hide' : 'view'} </button>
       </div>
       {showAll ? (<><div> url :
       {blog.url} 
