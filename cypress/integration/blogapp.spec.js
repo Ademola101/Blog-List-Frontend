2f382e838f5d9973aabb93beaa53a62ded50eab9
Ademola <ademolaogunmokun@yahoo.com>
2022-07-12 15:19:14 +0100

    succesfult login

--- cypress/integration/blogapp.spec.js
+++ cypress/integration/blogapp.spec.js
@@ -3,14 +3,34 @@ describe('Blog List app', function()  {
   beforeEach( function()  {
     cy.request('POST', 'http://localhost:3001/api/test/reset')
 
+    const user = {
+      username: 'sade',
+      password: 'werey'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users', user )
+
     cy.visit('http://localhost:3000')
 
   })
 
-  it('should open the front page', function()  {
+  it('Login form is shown', function()  {
 
     cy.visit('http://localhost:3000')
-    cy.contains('blogs')
+    cy.contains('login')
+  })
+
+  describe('Login', function() {
+
+    it('succeed with successful credentials', function() {
+
+      cy.get('#username').type('sade')
+      cy.get('#password').type('werey')
+      cy.contains('login').click()
+      cy.contains('sade logged in')
+
+    })
+
+
   })
 
 })
--- src/App.js
+++ src/App.js
@@ -118,11 +118,11 @@ const App = () => {
 
         <div>
           <label htmlFor="username">Username </label> <br />
-          <input type="text" name="username" value={username} onChange = {({ target }) => setUsername(target.value)}/>
+          <input type="text" name="username" id='username' value={username} onChange = {({ target }) => setUsername(target.value)}/>
         </div>
         <div>
           <label htmlFor="password">password</label> <br />
-          <input type="password" name="password" value={password} onChange = {({ target }) => setPassword(target.value)}/>  <br />
+          <input type="password" name="password" value={password} id = 'password' onChange = {({ target }) => setPassword(target.value)}/>  <br />
           <button>
     login
           </button>
